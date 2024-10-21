b6015f34c2aed266c952cd41868c5103e54ce140
Victor Lorencio <victorlorencio@gmail.com>
2024-10-21 18:01:49 +0200

    Inicio de proyecto Catalogo de productos con Spring

---
+++ src/main/java/vl/catalogoProductos/CatalogoProductosApplication.java
@@ -0,0 +1,13 @@
+package vl.catalogoProductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProductosApplication.class, args);
+	}
+
+}
